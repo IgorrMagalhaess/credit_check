e45a23536ca0a9605445bc8a4fd589e578b4dd1c
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-11-29 10:33:17 -0500

    Add credit check

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,6 +1,26 @@
 card_number = "5541808923795240"
 
-# Your Luhn Algorithm Here
+def credit_check(card_number)
+  characters = card_number.chars
+  numbers = characters.map(&:to_i)
+  doubles = numbers.map.with_index{ |n, i| i.even? ? n*2 : n}
+  single_digits_doubles = doubles.map { |n| n >= 10 ? n.digits.sum : n }
+  sum = single_digits_doubles.sum
+
+  if sum % 10 == 0
+    "The number #{card_number} is valid!"
+  else
+    "The number #{card_number} is invalid!"
+  end
+end
+
+puts credit_check("5541808923795240")
+puts credit_check("4024007136512380")
+puts credit_check("6011797668867828")
+puts credit_check("5541801923795240")
+puts credit_check("4024007106512380")
+puts credit_check("6011797668868728") 
+
 
 # Output
 ## If it is valid, print "The number [card number] is valid!"
