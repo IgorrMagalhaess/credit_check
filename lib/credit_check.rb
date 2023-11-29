f9656875682e705198ff8370beffffdcc45e9eeb
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-11-29 16:22:54 -0500

    Include comments

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,11 +1,11 @@
 card_number = "5541808923795240"
 
 def credit_check(card_number)
-  characters = card_number.chars
-  numbers = characters.map(&:to_i)
-  doubles = numbers.map.with_index{ |n, i| i.even? ? n*2 : n}
-  single_digits_doubles = doubles.map { |n| n >= 10 ? n.digits.sum : n }
-  sum = single_digits_doubles.sum
+  characters = card_number.chars #transform into an array
+  numbers = characters.map(&:to_i) #transform string into integers
+  doubles = numbers.map.with_index{ |n, i| i.even? ? n*2 : n} #double every other one
+  single_digits_doubles = doubles.map { |n| n >= 10 ? n.digits.sum : n } #sum doubles that have two digits
+  sum = single_digits_doubles.sum #sum array
 
   if sum % 10 == 0
     "The number #{card_number} is valid!"
